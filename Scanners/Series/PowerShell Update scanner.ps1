c3eeeb10b69cbfb9c814cdce5fd65ea7e4c5ab25
Neipas09 <58871039+Neipas09@users.noreply.github.com>
2019-12-13 22:19:45 -0600

    Update PowerShell Update scanner.ps1

--- Scanners/Series/PowerShell Update scanner.ps1
+++ Scanners/Series/PowerShell Update scanner.ps1
@@ -1,1 +1,5 @@
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py","Absolute Series Scanner.py")
+$url = "https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py"
+$output = "$PSScriptRoot\Absolute Series Scanner.py"
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
